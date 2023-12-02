a059d480855c7c90d7bd7563002defc1969e62da
DrDab <duvictor514@gmail.com>
2023-12-02 09:02:46 -0800

    Add Day2_1

---
+++ day2/Day2_1.java
@@ -0,0 +1,48 @@
+package day2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Day2_1 {
+    private static final int RL = 12;
+    private static final int GL = 13;
+    private static final int BL = 14;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("inputs/2.in"));
+        
+        long sum = 0L;
+
+        while (sc.hasNextLine()) {
+            StringTokenizer st = new StringTokenizer(sc.nextLine());
+            st.nextToken();
+            int game = Integer.parseInt(st.nextToken().replaceAll(":", ""));
+            int mr = 0;
+            int mg = 0;
+            int mb = 0;
+
+            while (st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+                String color = st.nextToken();
+
+                if (color.startsWith("blue")) {
+                    mb = Math.max(num, mb);
+                } else if (color.startsWith("red")) {
+                    mr = Math.max(num, mr);
+                } else {
+                    mg = Math.max(num, mg);
+                }
+            }
+
+            if (mr <= RL && mb <= BL && mg <= GL) {
+                sum += game;
+            }
+
+
+        }
+
+        System.out.println(sum);
+    }
+}
