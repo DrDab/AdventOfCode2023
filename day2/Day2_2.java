cfe0239aead8ac5cad1d6801f4f652e159f05424
DrDab <duvictor514@gmail.com>
2023-12-02 09:06:21 -0800

    Create Day2_2.java

---
+++ day2/Day2_2.java
@@ -0,0 +1,40 @@
+package day2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Day2_2 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("inputs/2.in"));
+        
+        long sum = 0L;
+
+        while (sc.hasNextLine()) {
+            StringTokenizer st = new StringTokenizer(sc.nextLine());
+            st.nextToken();
+            int game = Integer.parseInt(st.nextToken().replaceAll(":", ""));
+            int mr = 0;
+            int mg = 0;
+            int mb = 0;
+
+            while (st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+                String color = st.nextToken();
+
+                if (color.startsWith("blue")) {
+                    mb = Math.max(num, mb);
+                } else if (color.startsWith("red")) {
+                    mr = Math.max(num, mr);
+                } else {
+                    mg = Math.max(num, mg);
+                }
+            }
+
+            sum += (mr * mg * mb);
+        }
+
+        System.out.println(sum);
+    }
+}
